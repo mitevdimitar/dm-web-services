993a29a6d50c7205dac2457b7d584a2a1fc1a671
Dimitar Mitev <mitev.dimitar@gmail.com>
2022-03-25 08:31:46 +0200

    add mui styles

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,20 @@
+import Grid from '@mui/material/Grid';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: 'red'
+  },
+});
+
+
 function Home() {
+    const classes = useStyles();
+
     return (
-        <div id="home" className="full-height">
+        <Grid id="home" className={`${classes.root} full-height`}>
             Home
-        </div>
+        </Grid>
     )
 }
 
