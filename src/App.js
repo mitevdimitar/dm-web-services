87bf8375441cb61162a6369e96276a5e88d57964
Dimitar Mitev <mitev.dimitar@gmail.com>
2022-03-28 08:36:35 +0300

    add app bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './Pages/Home';
 import './App.css';
+import MenuBar from "./Components/AppBar";
 
 function App() {
   return (
     <div className="App">
+     <MenuBar />
       <Home />
     </div>
   );
