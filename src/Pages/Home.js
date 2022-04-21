34646e9f2f989c1e2e4b3df660b117b7005c1533
Dimitar Mitev <mitev.dimitar@gmail.com>
2022-04-21 08:38:05 +0300

    replace home page background image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,14 @@
 import Grid from '@mui/material/Grid';
 import { makeStyles } from '@mui/styles';
-import background from "../Assets/img/home.jpg";
+import background from "../Assets/img/home.jpeg";
 
 const useStyles = makeStyles({
   root: {
     backgroundImage: `url(${background})`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
-    backgroundPositionX: "right"
+    backgroundPositionX: "right",
+    position: "relative"
     //if mobile, use backgroundPositionY: 20% either with Size cover or 100% (and extra space for the services)
   },
 });
@@ -18,7 +19,6 @@ function Home() {
 
     return (
         <Grid id="home" className={`${classes.root} full-height`}>
-            
         </Grid>
     )
 }
