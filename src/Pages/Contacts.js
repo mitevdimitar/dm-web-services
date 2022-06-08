21272d6fe1bf8385afdefecdc507ea2c57e5573e
Dimitar Mitev <mitev.dimitar@gmail.com>
2022-06-08 07:57:13 +0300

    contact details

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,6 +1,10 @@
 import Grid from '@mui/material/Grid';
 import { makeStyles } from '@mui/styles';
 import ContactForm from '../Components/ContactForm';
+import Typography from '@mui/material/Typography';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import HomeIcon from '@mui/icons-material/Home';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -9,10 +13,20 @@ const useStyles = makeStyles(theme => ({
     background: "#a5a2a2",
     color: "white"
   },
-  contactForm: {
+  messageForm: {
     background: "white",
     borderRadius: 16,
     color: theme.palette.primary.main
+  },
+  contactHeader: {
+    marginTop: "7em !important",
+    marginBottom: "3em"
+  },
+  contactsForm: {
+    maxHeight: "50vh"
+  },
+  contactRow: {
+    marginBottom: "1em !important"
   }
 }));
 
@@ -21,12 +35,42 @@ function Contacts() {
     const classes = useStyles();
 
     return (
-        <Grid container id="services" className={classes.root} justifyContent="space-around">
-            <Grid container item xs={4} alignItems="center" justifyContent="center" className={classes.contactForm}>
+        <Grid container id="services" className={classes.root} justifyContent="space-around" /* alignItems="center" */>
+            <Grid container item xs={4} alignItems="center" justifyContent="center" className={classes.messageForm}>
               <ContactForm />
             </Grid>
-            <Grid container item xs={4} alignItems="center" justifyContent="center">
-              Contacts
+            <Grid container item xs={4} justifyContent="center" className={classes.contactsForm}>
+              <Grid container justifyContent="center" className={classes.contactHeader}>
+                <Typography component="h1" variant="h5">
+                  Contact us
+                </Typography>
+              </Grid>
+              <Grid /* container */ style={{width: "60%"}}>
+                <Grid container item className={classes.contactRow}>
+                  <Grid item xs={2}>
+                    <PhoneIphoneIcon />
+                  </Grid>
+                  <Grid item xs={10}>
+                    +359 888 656524
+                  </Grid>
+                </Grid>
+                <Grid container item className={classes.contactRow}>
+                  <Grid item xs={2}>
+                    <MailOutlineIcon />
+                  </Grid>
+                  <Grid item xs={10}>
+                    mitev.dimitar@gmail.com
+                  </Grid>
+                </Grid>
+                <Grid container item className={classes.contactRow}>
+                  <Grid item xs={2}>
+                    <HomeIcon />
+                  </Grid>
+                  <Grid item xs={10}>
+                    Varna, Bulgaria
+                  </Grid>
+                </Grid>
+              </Grid>
             </Grid>
         </Grid>
     )
