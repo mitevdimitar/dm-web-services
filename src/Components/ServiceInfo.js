fb929ad1f7f02fdb599c8b44b7ea087bad69efb2
Dimitar Mitev <mitev.dimitar@gmail.com>
2022-06-13 08:00:38 +0300

    export service info into component

---
+++ src/Components/ServiceInfo.js
@@ -0,0 +1,49 @@
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import { makeStyles } from '@mui/styles';
+import frontEnd from "../Assets/img/front-end.jpg";
+
+const useStyles = makeStyles({
+    infoContainer: {
+      height: "100%",
+      margin: "30px !important",
+    },
+    header: {
+      marginBottom: "20px !important"
+    },
+  });
+
+function ServiceInfo() {
+    const classes = useStyles();
+
+    return (
+        <Grid container item className={classes.infoContainer}>
+            <Grid container item alignItems="center" className={classes.header}>
+                <Avatar aria-label="front-end-service" alt="computer" src={frontEnd} sx={{ width: 25, height: 25, marginRight: 2 }}/>
+                <Typography variant="h5">
+                    Front-end services
+                </Typography>
+            </Grid>
+            <Typography align="justify">
+                <strong>ReactJS </strong>is renowned for its extensibility, adjustability and convenience. 
+                Many of the developers acknowledge it one of the best JavaScript open source libraries. 
+                It makes possible the creation of such complex software as web browser and mobile application 
+                user interfaces that accommodate to the strictest standards.
+            </Typography>
+            <ul>
+                <li>Service 1</li>
+                <li>Service 2</li>
+                <li>Service 3</li>
+            </ul>
+            <Grid container>
+                <Button variant="contained" >
+                    Find out more
+                </Button>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default ServiceInfo;
--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,15 +1,13 @@
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
 import { makeStyles } from '@mui/styles';
 import { useState } from 'react';
 import ServiceCard from '../Components/ServiceCard';
 import frontEnd from "../Assets/img/front-end.jpg";
 import seo from "../Assets/img/seo.jpg";
 import onlineCouching from "../Assets/img/online-couching.jpg";
-import Button from '@mui/material/Button';
 import { SERVICES } from '../Utils/constants';
 import useIsInViewport from "use-is-in-viewport";
+import ServiceInfo from '../Components/ServiceInfo';
 
 const useStyles = makeStyles({
   root: {
@@ -24,13 +22,6 @@ const useStyles = makeStyles({
     //backgroundColor: "#586062",
     color: "white",
     padding: "0 15px"
-  },
-  infoContainer: {
-    height: "100%",
-    margin: "30px !important",
-  },
-  header: {
-    marginBottom: "20px !important"
   },
   slideRight: {
     animation: "$slideInRight 1.5s forwards",
@@ -93,30 +84,7 @@ function Services() {
               </Grid>
             ) /* : null */}
             {info && (
-              <Grid container item className={classes.infoContainer}>
-                <Grid container item alignItems="center" className={classes.header}>
-                  <Avatar aria-label="front-end-service" alt="computer" src={frontEnd} sx={{ width: 25, height: 25, marginRight: 2 }}/>
-                  <Typography variant="h5">
-                    Front-end services
-                  </Typography>
-                </Grid>
-                <Typography align="justify">
-                  <strong>ReactJS </strong>is renowned for its extensibility, adjustability and convenience. 
-                  Many of the developers acknowledge it one of the best JavaScript open source libraries. 
-                  It makes possible the creation of such complex software as web browser and mobile application 
-                  user interfaces that accommodate to the strictest standards.
-                </Typography>
-                <ul>
-                  <li>Service 1</li>
-                  <li>Service 2</li>
-                  <li>Service 3</li>
-                </ul>
-                <Grid container>
-                  <Button variant="contained" >
-                    Find out more
-                  </Button>
-                </Grid>
-              </Grid>
+              <ServiceInfo />
             )}
         </Grid>
     )
